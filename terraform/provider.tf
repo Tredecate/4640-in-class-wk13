3672a61387b74fc07c0fe41528907a1ac8575622
Tredecate <thomasdezwart@gmail.com>
2026-03-31 21:59:23 -0700

    Add s3 backend to `provider.tf` and format

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,9 +1,17 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 6.0"
     }
+  }
+
+  backend "s3" {
+    bucket       = "4640-wk13-lab-99981"
+    key          = "terraform.tfstate"
+    region       = "us-west-2"
+    encrypt      = true
+    use_lockfile = true
   }
 }
 
